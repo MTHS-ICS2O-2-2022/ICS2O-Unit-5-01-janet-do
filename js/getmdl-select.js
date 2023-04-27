ad2c412b12c7aabe44db6fa99cc39ac7808918de
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-27 16:44:46 +0000

    sync

--- js/getmdl-select.js
+++ js/getmdl-select.js
@@ -1,3 +1,8 @@
+// Copyright (c) 2020 Janet Do All rights reserved
+//
+// Created by: Janet Do
+// Created on: Sep 2020
+// This file generates a random number generator
 {
   ;("use strict")
   ;(function () {
